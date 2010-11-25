ba6f42e404802cd8db2e83d983268ec6149b7112
Benson Che <coffeeaddictsunited@gmail.com>
2010-11-25 14:59:43 -0900

    links formatting

--- public/index.php
+++ public/index.php
@@ -2,30 +2,32 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="description" content="ABC: Asian Bible Church of Anchorage, AK" />
+        <meta name="Keywords" content="Asian Bible Church Chinese Anchorage Alaska AK Christian" />
         <title>Asian Bible Church</title>
 
         <link rel="stylesheet" type="text/css" href="abc.css" />
     </head>
 
     <body>
-        <div class="content" style="position: relative;">
-            <img width="100%" height="100px" src="images/logo.jpg" />
+        <div class="body" style="position: relative;">
+            <img width="100%" height="100px" src="images/logo.jpg" alt="Logo" />
             <table class="menu">
                 <tr>
                     <td>
-                        Home
+                        <a href="">Home</a>
                     </td>
                     <td>
-                        Activities
+                        <a href="">Activities</a>
                     </td>
                     <td>
-                        Resources
+                        <a href="">Resources</a>
                     </td>
                     <td>
-                        Contact
+                        <a href="">Contact</a>
                     </td>
                     <td>
-                        中文
+                        <a href="">中文</a>
                     </td>
                 </tr>
             </table>
