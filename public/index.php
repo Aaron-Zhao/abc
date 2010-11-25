af0cd7e00c118d5d3fb58a1ff47c83e6bbff4770
Benson Che <coffeeaddictsunited@gmail.com>
2010-11-25 14:44:33 -0900

    basic layout for webpage in index.php

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Asian Bible Church</title>
+
+        <link rel="stylesheet" type="text/css" href="abc.css" />
+    </head>
+
+    <body>
+        <div class="content" style="position: relative;">
+            <img width="100%" height="100px" src="images/logo.jpg" />
+            <table class="menu">
+                <tr>
+                    <td>
+                        Home
+                    </td>
+                    <td>
+                        Activities
+                    </td>
+                    <td>
+                        Resources
+                    </td>
+                    <td>
+                        Contact
+                    </td>
+                    <td>
+                        中文
+                    </td>
+                </tr>
+            </table>
+
+            <h1>Welcome to Asian Bible Church</h1>
+
+        </div>
+    </body>
+</html>
