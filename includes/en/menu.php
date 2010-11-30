2915edff9672a772fff2f50500126a327313cc09
Benson Che <coffeeaddictsunited@gmail.com>
2010-11-30 01:36:54 -0900

    Added banner

--- includes/en/menu.php
+++ includes/en/menu.php
@@ -2,7 +2,7 @@
 
     <body>
         <div class="body" style="position: relative;">
-            <img width="100%" height="100px" src="images/logo.jpg" alt="Logo" />
+            <img width="100%" height="100px" src="../shared/images/ABC_Banner_en.jpg" alt="Logo" />
             <table class="menu">
                 <tr>
                     <td>
