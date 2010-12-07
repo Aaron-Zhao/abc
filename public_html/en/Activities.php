59ba41d7af4517c18931a8dfaf28ecef58abb16d
Benson Che <coffeeaddictsunited@gmail.com>
2010-12-06 23:27:51 -0900

    Removed xmas

--- public_html/en/Activities.php
+++ public_html/en/Activities.php
@@ -35,35 +35,6 @@ class page extends template
                     </table>
                 </td>
             </tr>
-            <tr>
-                <td>
-                    <table>
-                        <tr>
-                            <td colspan="3" >
-                                <h2>Christmas Eve Singspiration</h2>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td align="center" style="padding-left: 20px; width: 200px;" >
-                                <p>Date: December 24th, 2010</p>
-                                <p>Time: 6:45 PM - 8:00 PM</p>
-                                <p>Location: Rob &amp; Lucy's home</p>
-                                <p>12741 Rivulet Cir.</p>
-                                <p>(907) 929-6030</p>
-                                <p>Please R.S.V.P.</p>
-                            </td>
-                            <td width="50px" />
-                            <td align="center">
-                                <a href="../shared/images/christmas_poster.jpg">
-                                    <img alt="poster" src="../shared/images/christmas_poster_thumb.jpg" width="200px" height="259px" style="border: 1px solid #BDBDBD" />
-                                </a>
-
-                                <p>Click on the image for the fullsize image</p>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
         </table>
 <?php
     }
--- public_html/zh/Activities.php
+++ public_html/zh/Activities.php
@@ -35,35 +35,6 @@ class page extends template
                     </table>
                 </td>
             </tr>
-            <tr>
-                <td>
-                    <table>
-                        <tr>
-                            <td colspan="3" >
-                                <h2>聖誕節前夜歡唱</h2>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td align="center" style="padding-left: 20px; width: 200px;" >
-                                <p>日期：二零一零年十二月二十四日</p>
-                                <p>時間：晚上六時四十五分至八時</p>
-                                <p>地點：牧師家</p>
-                                <p>12741 Rivulet Cir.</p>
-                                <p>(907) 929-6030</p>
-                                <p>敬請賜覆</p>
-                            </td>
-                            <td width="50px" />
-                            <td align="center">
-                                <a href="../shared/images/christmas_poster.jpg">
-                                    <img alt="poster" src="../shared/images/christmas_poster_thumb.jpg" width="200px" height="259px" style="border: 1px solid #BDBDBD" />
-                                </a>
-
-                                <p>Click on the image for the fullsize image</p>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
         </table>
 <?php
     }
