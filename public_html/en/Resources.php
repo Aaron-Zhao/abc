d142c3ecd62e3f2df882c3e44064d0bffa3330bc
Benson Che <coffeeaddictsunited@gmail.com>
2010-12-21 17:26:05 -0900

    Added resources.

--- public_html/en/Resources.php
+++ public_html/en/Resources.php
@@ -15,6 +15,10 @@ class page extends template
     {
 ?>
         <h1>Resources</h1>
+
+        <p><a href="../shared/resources/statement.doc">Statement of Faith</a></p>
+        <p><a href="../shared/resources/Good_Friday.mp3">Good Friday Sermon</a></p>
+        <p><a href="../shared/resources/Father.ram">Father's Day Sermon (English and Chinese)</a></p>
 <?php
     }
 }
--- public_html/zh/Resources.php
+++ public_html/zh/Resources.php
@@ -15,6 +15,10 @@ class page extends template
     {
 ?>
         <h1>資源</h1>
+
+        <p><a href="../shared/resources/statement.doc">信仰聲明</a></p>
+        <p><a href="../shared/resources/Good_Friday.mp3">受難日講道</a></p>
+        <p><a href="../shared/resources/Father.ram">父親節講道 （中英）</a></p>
 <?php
     }
 }
